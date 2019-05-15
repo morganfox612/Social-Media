bdf1d86bef41405f921b4f9648cd94db30c19553
Morgan Fox <morganfox61297@gmail.com>
2019-05-15 15:56:56 -0400

    class exercise

--- main.js
+++ main.js
@@ -3,12 +3,19 @@ class User {
   this.email = email
   this.password = password
   this.inbox = []
+  this.blockedlist = []
 }
 sendMessage(recipient) {
   let msg = new Message()
-  console.log('Message sent: ' + msg.content)
+
+ if (!recipient.blockedlist.includes(this.email)){
+
   recipient.inbox.push(msg)
 }
+}
+
+blocked(user) {
+  this.blockedlist.push(user.email)
 }
 
 class Message {
@@ -19,3 +26,33 @@ class Message {
 
 user1 = new User('morganfox61297@gmail.com', '11111111')
 user2 = new User('fox@gmail.com', '11111111')
+user3 = new User('blank@gmail.com', '11111111')
+
+let user1form = document.getElementById('user1-form')
+let user2inbox = document.getElementById('user2-inbox')
+window.addEventListener('keydown', function(event){
+  if (event.which  == 13) {
+    sendMessage()
+  }
+    })
+
+function blockUser() {
+let blocked = document.getElementById('block-btn')
+blocked.addEventListener('block', function(event) {
+  alert('You have blocked this user')
+})
+}
+
+function sendMessage() {
+  let text = document.getElementById('msg-content')
+  let p =document.createElement('p')
+  p.innerText = text.value
+  user2inbox.appendChild(p)
+  text.value =''
+  text.focus()
+}
+
+user1form.addEventListener('submit', function(event) {
+  event.preventDefault()
+  sendMessage()
+})
